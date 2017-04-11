e4a5470f9c87a0418dbdd3de4178231ff8aeaf7a
rickybas <rickybas12@gmail.com>
2017-04-11 01:35:18 +0100

    - Switched around how numbers are placed (high to low)  - Scaled the ladder to fit the window height  - Enabled minus numbers  - Implemented touch screen support

--- web/main.dart
+++ web/main.dart
@@ -1,184 +1,164 @@
 import 'dart:html';
 import 'dart:math';
 import 'dart:async';
+import 'package:dragula/dragula.dart';
 
 int numSpaces;
-int numRange;
+const int defMaxRandomNumber = 5;
+const int defMinRandomNumber = -5;
+
+int maxRandomNumber;
+int minRandomNumber;
+int numberWithMaxDigits;
+
 final int digitHeight = 70;
-final int spaceHeightPx = 80;
-int ladderHeightPx;
-const int randAnimationWaitTime = 1000*1;
-final defNumSpaces = 6;
-final defRanNum = defNumSpaces+3;
+const int randAnimationWaitTime = 1000;
 
-
-int numRecords=0;
-List<int> numLadder = new List(numSpaces);
+int numRecords = 0;
+List<int> numLadder;
 
 bool droppedNumber = true;
 bool animation = false;
 
+final InputElement maxRandomNumberInput = querySelector("#maxnumrange");
+final InputElement minRandomNumberInput = querySelector("#minnumrange");
+
+List<String> recordIdsDropped = [];
+List<String> spacesFull = [];
+
+Drake drag;
+
 void main() {
   print("Number Ladder Game");
   print("Source Code: https://github.com/rickybas/number-ladder-game");
+
   setVariables();
   updateButton();
   drawLadder();
   drawNumberGenerator();
+
+  drag = dragula(new List.from(querySelectorAll('.space'))
+    ..add(querySelector('#generatednumbers')),
+      removeOnSpill: true,
+      copy: true,
+      accepts: (el, container, handle, sibling) =>
+      (!spacesFull.contains(container.dataset["num"])),
+      moves: (el, container, handle, sibling) =>
+      (handle.className == 'record')
+          && (!recordIdsDropped.contains(handle.id))
+  )
+    ..onOver((Element el, Element container, Element source) {
+      if (container.className == "space")
+        container.style.backgroundColor = "red";
+    })
+    ..onOut((Element el, Element container, Element source) {
+      if (container.className == "space") container.style.backgroundColor = "";
+    })
+    ..onDrop(drop);
 }
 
-updateButton(){
+updateButton() {
   querySelector("#update")
-    ..onClick.listen((e){
-      InputElement numspacesInput = querySelector("#numspaces");
-      InputElement numrangeInput = querySelector("#numrange");
-
-      if(numspacesInput.value == null || numspacesInput.value == "" ||
-          numrangeInput.value == null || numrangeInput.value == ""){
+    ..onClick.listen((e) {
+      if (minRandomNumberInput.value == null ||
+          minRandomNumberInput.value == "" ||
+          maxRandomNumberInput.value == null ||
+          maxRandomNumberInput.value == "") {
         window.location.href = getPathFromUrl(window.location.href)
-            + "?s=" + "20" + "&"
-            + "r=" + "30";
+            + "?maxr=" + defMaxRandomNumber.toString() + "&"
+            + "minr=" + defMinRandomNumber.toString();
       }
 
       window.location.href = getPathFromUrl(window.location.href)
-          + "?s=" + numspacesInput.value + "&"
-          + "r=" + numrangeInput.value;
+          + "?maxr=" + maxRandomNumberInput.value + "&"
+          + "minr=" + minRandomNumberInput.value;
     });
 }
 
-setVariables(){
-  InputElement numspacesInput = querySelector("#numspaces");
-  InputElement numrangeInput = querySelector("#numrange");
-
-  int min = 2;
-  int max = 30;
-
-  int spaces;
+setVariables() {
   try {
-    spaces = int.parse(getQueryVariable("s"));
-  } catch(e){
-    spaces = defNumSpaces;
+    maxRandomNumber = int.parse(getQueryVariable("maxr"));
+  } catch (e) {
+    maxRandomNumber = defMaxRandomNumber;
+    minRandomNumber = defMinRandomNumber;
+  } finally {
+    try {
+      minRandomNumber = int.parse(getQueryVariable("minr"));
+    } catch (e) {
+      maxRandomNumber = defMaxRandomNumber;
+      minRandomNumber = defMinRandomNumber;
+    }
   }
 
-  int range;
-  try {
-    range = int.parse(getQueryVariable("r"));
-  } catch(e){
-    range = defRanNum;
+  if(maxRandomNumber<minRandomNumber){
+    maxRandomNumber = defMaxRandomNumber;
+    minRandomNumber = defMinRandomNumber;
   }
 
-  if(spaces>max || spaces<min) spaces=defNumSpaces;
-  if(spaces>=range) range=spaces+3;
-  if(range<min) range = defRanNum;
+  numSpaces = (0.75*(maxRandomNumber - minRandomNumber).abs()).floor();
 
-  numSpaces = spaces;
-  numspacesInput.value = numSpaces.toString();
+  if (numSpaces <= 2 || numSpaces >= 35) {
+    maxRandomNumber = defMaxRandomNumber;
+    minRandomNumber = defMinRandomNumber;
+    numSpaces = (0.75*(maxRandomNumber - minRandomNumber).abs()).floor();
+  }
 
-  numRange = range;
-  numrangeInput.value = numRange.toString();
+  numLadder = new List(numSpaces);
 
-  ladderHeightPx = numSpaces * spaceHeightPx;
+  if (numSpaces <= 10)
+    querySelector("#ladder").style.fontSize = "25px";
+  else if (numSpaces > 10 && numSpaces <= 20)
+    querySelector("#ladder").style.fontSize = "20px";
+  else if (numSpaces > 20)
+    querySelector("#ladder").style.fontSize = "15px";
+  else
+    querySelector("#ladder").style.fontSize = "10px";
+
+  maxRandomNumberInput.value = maxRandomNumber.toString();
+  minRandomNumberInput.value = minRandomNumber.toString();
+  numberWithMaxDigits = ((maxRandomNumber.abs() > minRandomNumber.abs())
+      ? maxRandomNumber.abs()
+      : minRandomNumber.abs());
 }
 
-drawNumberGenerator(){
-  int numDigits = (log(numRange)/log(10)).floor()+1;
+drawNumberGenerator() {
+  int numDigits = (log(numberWithMaxDigits) / log(10)).floor() + 1;
 
-  var digitsEl = querySelector("#digits");
-  for(int j=0; j<numDigits; j++){
+  Element digitsEl = querySelector("#digits");
+  for (int j = 0; j < numDigits; j++) {
     digitsEl.append(new DivElement()
       ..className = "number"
-      ..id = "num"+j.toString());
+      ..id = "num" + j.toString());
   }
 
-
-  int j=0;
-  for (var el in querySelectorAll(".number")) {
-    for(var i = 0; i < 10; i++){
+  int j = 0;
+  for (Element el in querySelectorAll(".number")) {
+    for (int i = 0; i < 10; i++) {
       el.append(new DivElement()
-        ..classes=["digit", "d"+j.toString()]
-        ..id="dig"+i.toString()
-        ..text=i.toString());
+        ..classes = ["digit", "d" + j.toString()]
+        ..id = "dig" + i.toString()
+        ..text = i.toString());
     }
     j++;
   }
-  var numspaces = querySelector("#numspaces");
-  var numrange = querySelector("#numrange");
 
-
-
-  var btn = querySelector("#click");
+  Element btn = querySelector("#click");
   btn.onClick.listen(genRandNum);
 }
 
-drawLadder(){
-  var ladder = querySelector("#ladder")
-    ..style.height = ladderHeightPx.toString() +"px";
-  for(int i=0; i<numSpaces; i++){
+drawLadder() {
+  Element ladder = querySelector("#ladder")
+    ..style.height = (90 - (2 * (90 / numSpaces))).toString() + "%";
+  for (int i = 0; i < numSpaces; i++) {
     ladder.append(new DivElement()
-      ..className="space"
-      ..style.height = spaceHeightPx.toString() + "px"
+      ..className = "space"
+      ..style.height = (100 / numSpaces).toString() + "%"
       ..style.borderTop = "4px solid black"
-      ..dataset={"num": i.toString()}
-      ..onDragOver.listen(allowDrop)
-      ..onDragLeave.listen(leave)
-      ..onDrop.listen(drop)
+      ..dataset = {"num": i.toString()}
     );
   }
   querySelectorAll(".legs")
-    ..style.height = (ladderHeightPx/numSpaces).toString() + "px";
-}
-
-allowDrop(e){
-  e.target.id = "hoveredbox";
-  e.preventDefault();
-}
-
-leave(e){
-  e.target.id = "emptybox";
-}
-
-drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
-}
-
-drop(ev) {
-  ev.preventDefault();
-  ev.target.id = null;
-  var data = ev.dataTransfer.getData("text");
-  var nodeCopy = document.getElementById(data).clone(true);
-  nodeCopy.className="";
-  nodeCopy.draggable=false;
-  nodeCopy.style.fontSize=(ladderHeightPx*0.6/numSpaces).toString() + "px";
-  nodeCopy.style.height=spaceHeightPx.toString() + "px";
-
-  if(ev.target.innerHtml != "" && ev.target.innerHtml != null){
-    window.alert("Number already in that space");
-    return;
-  }
-  ev.target.append(nodeCopy);
-  document.getElementById(data).draggable=false;
-
-  for(var el in querySelector("#generatednumbers").children){
-    el.draggable=false;
-    el.style.cursor = "not-allowed";
-  }
-
-  if(!checkNumbers(int.parse(document.getElementById(data).text), int.parse(ev.target.dataset["num"]))){
-    window.alert("Lost - score: " + calScore().toString());
-    window.location.reload();
-  }
-  numLadder[int.parse(ev.target.dataset["num"])] = int.parse(document.getElementById(data).text);
-
-  int numAl = 0;
-  for(var num in numLadder){
-    if(num != null) numAl++;
-  }
-  if(numAl==numSpaces){
-    window.alert("Won! - score: " + numSpaces.toString());
-    window.location.reload();
-  }
-
-  droppedNumber=true;
+    ..style.height = (45 / numSpaces).toString() + "%";
 }
 
 int getRandomInt(min, max) {
@@ -186,57 +166,97 @@ int getRandomInt(min, max) {
   return (min + rng.nextInt(max - min));
 }
 
-genRandNum(e){
-  if(animation) return;
-  if(!droppedNumber){
+genRandNum(e) {
+  if (animation) return;
+  if (!droppedNumber) {
     window.alert("Please drop number on ladder");
     return;
   }
 
   int number;
   bool foundNum = false;
-  while(!foundNum){
-    number = getRandomInt(0, numRange)+1;
-    if(!numLadder.contains(number)) foundNum=true;
+  while (!foundNum) {
+    number = getRandomInt(minRandomNumber, maxRandomNumber + 1);
+    if (!numLadder.contains(number)) foundNum = true;
   }
 
-  int numDigits = (log(numRange)/log(10)).floor()+1;
-  var pad = "0"*numDigits;
-  var input = pad.substring(0, pad.length - number.toString().length) + number.toString();
-  for(int i = 0; i < input.length; i++){
-    var digit = int.parse(input[i]);
-    for (var el in querySelectorAll(".d" + i.toString())){
-      el.style.top = (-1*digit*digitHeight).toString() + "px";
+  int numDigits = (log(numberWithMaxDigits) / log(10)).floor() + 1;
+  String pad = "0" * numDigits;
+  String input = pad.substring(0, pad.length - number
+      .abs()
+      .toString()
+      .length) + number.abs().toString();
+  for (int i = 0; i < input.length; i++) {
+    int digit = int.parse(input[i]);
+    for (Element el in querySelectorAll(".d" + i.toString())) {
+      el.style.top = (-1 * digit * digitHeight).toString() + "px";
     }
+  }
 
+  Element negSign = querySelector("#negsign");
+  if (number.isNegative) {
+    negSign.style.top = "0px";
+  } else {
+    negSign.style.top = "70px";
   }
 
   animation = true;
-  const time = const Duration(milliseconds:(randAnimationWaitTime));
+  const time = const Duration(milliseconds: (randAnimationWaitTime));
   new Timer(time, () => addNewNumber(number));
-  droppedNumber=false;
-
+  droppedNumber = false;
 }
 
-addNewNumber(int number){
+drop(Element el, Element space, __, ___) {
+  el.className = "";
+  el.style.height = (100 / (numSpaces)).toString() + "%";
+
+//  if(space.children.length > 0 && space.children[0].id != el.id){
+//    window.alert("Number already in that space");
+//    return;
+//  }
+
+  for (Element el in querySelector("#generatednumbers").children) {
+    el.draggable = false;
+    el.style.cursor = "not-allowed";
+  }
+
+  if (!checkNumbers(int.parse(el.text), int.parse(space.dataset["num"]))) {
+    window.alert("Lost - score: " + calScore().toString());
+    window.location.reload();
+  }
+  numLadder[int.parse(space.dataset["num"])] = int.parse(el.text);
+
+  int numAl = 0;
+  for (int num in numLadder) {
+    if (num != null) numAl++;
+  }
+  if (numAl == numSpaces) {
+    window.alert("Won! - score: " + numSpaces.toString());
+    window.location.reload();
+  }
+
+  droppedNumber = true;
+  recordIdsDropped.add(el.id);
+  spacesFull.add(space.dataset["num"]);
+}
+
+addNewNumber(int number) {
   checkAllPositions(number);
-  var generatednumbers = querySelector("#generatednumbers");
+  Element generatednumbers = querySelector("#generatednumbers");
   generatednumbers.insertBefore(new DivElement()
-    ..id="record"+numRecords.toString()
-    ..className="record"
-    ..text=number.toString()
-    ..draggable=true
-    ..style.height="50px"
-    ..onDragStart.listen(drag),
+    ..id = "record" + numRecords.toString()
+    ..className = "record"
+    ..text = number.toString()
+    ..style.height = "50px",
       generatednumbers.firstChild);
-  numRecords+=1;
+  numRecords += 1;
   animation = false;
 }
 
-checkAllPositions(int newNum){
-  for(int i=0; i<numSpaces; i++){
-    if(numLadder[i] != null) continue;
-    if(checkNumbers(newNum, i)){
+checkAllPositions(int newNum) {
+  for (int i = 0; i < numSpaces; i++) {
+    if (numLadder[i] != null) continue;
+    if (checkNumbers(newNum, i)) {
       return;
     }
   }
@@ -245,14 +265,14 @@ checkAllPositions(int newNum){
 }
 
 
-bool checkNumbers(int newNum, int newNumPos){
-  for(int i=newNumPos-1; i>=0; i--){
-    if(numLadder[i] != null && newNum > numLadder[i]){
+bool checkNumbers(int newNum, int newNumPos) {
+  for (int i = newNumPos - 1; i >= 0; i--) {
+    if (numLadder[i] != null && newNum < numLadder[i]) {
       return false;
     }
   }
-  for(int i=newNumPos+1; i!=numSpaces; i++){
-    if(numLadder[i] != null && newNum < numLadder[i]){
+  for (int i = newNumPos + 1; i != numSpaces; i++) {
+    if (numLadder[i] != null && newNum > numLadder[i]) {
       return false;
     }
   }
@@ -260,23 +280,23 @@ bool checkNumbers(int newNum, int newNumPos){
 }
 
 getQueryVariable(String variable) {
-  var query = window.location.search.substring(1);
-  var vars = query.split("&");
-  for (int i=0;i<vars.length;i++) {
-    var pair = vars[i].split("=");
-    if(pair[0] == variable){return pair[1];}
+  String query = window.location.search.substring(1);
+  List<String> vars = query.split("&");
+  for (int i = 0; i < vars.length; i++) {
+    List<String> pair = vars[i].split("=");
+    if (pair[0] == variable) return pair[1];
   }
-  return(false);
+  return false;
 }
 
 getPathFromUrl(String url) {
   return url.split("?")[0];
 }
 
-int calScore(){
+int calScore() {
   int score = 0;
-  for(int num in numLadder){
-    if(num != null) score++;
+  for (int num in numLadder) {
+    if (num != null) score++;
   }
   return score;
 }
